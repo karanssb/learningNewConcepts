a2c31f8d62932ebd87466dc0b35cd5be9f5e5cc9
karanssb <karanssb@gmail.com>
2023-08-30 09:40:10 -0500

    :wq :wq

---
+++ forLopp.java
@@ -0,0 +1,8 @@
+public class forLopp {
+    public static void main(String[] args) {
+        int n =23;
+        for(int i =1; i<=10;i++) {
+            System.out.println( n + " * " + i + " = "  + (n*i) );
+        }
+    }
+}
